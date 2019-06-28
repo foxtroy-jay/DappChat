930c0b93af6ae96d10c5ec5854557883c047d156
Thomas Luo <tluo9713@gmail.com>
2019-06-28 17:53:09 -0400

    theoretically deployable twittor

---
+++ contracts/Twittor.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.5.8;
+pragma experimental ABIEncoderV2;
+
+
+contract Twittor {
+
+mapping(address => string[]) private tweets;
+address[] private keyList;
+
+  constructor() public {
+  }
+
+  function getMessageLength() public view returns (uint){
+      return tweets[msg.sender].length;
+  }
+
+  function setMessage(string memory newMessage) public {
+    if(tweets[msg.sender].length == 0){
+        keyList.push(msg.sender);
+    }
+
+    tweets[msg.sender].push(newMessage);
+  }
+
+  function getMessageByIndex(uint index) external view returns (string memory) {
+    return tweets[msg.sender][index];
+  }
+
+  function fetchUserTweets(address user) public view returns (string[] memory) {
+    return tweets[user];
+  }
+
+  function getKeys() public view returns (address[] memory) {
+        return keyList;
+
+  }
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const Twittor = artifacts.require('Twittor');
+
+module.exports = function(deployer) {
+  deployer.deploy(Twittor);
+};
