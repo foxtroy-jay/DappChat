bb4683398b8b269a4b63bb222b9aad07bb136ff0
TristanWatanabe <tristan.watanabe@gmail.com>
2019-06-28 20:39:24 -0400

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -71,7 +71,6 @@ module.exports = {
         ),
       network_id: 4, // Ropsten's id
       gas: 4500000,
-      gasPrice: 10000000000, // Ropsten has a lower block limit than mainnet
       confirmations: 1, // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 5, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
