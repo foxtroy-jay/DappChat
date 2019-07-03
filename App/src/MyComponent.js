e214cb7936fde208efce717657f9f1d77ebd08e3
Tristan Watanabe <tristan.watanabe@gmail.com>
2019-07-03 11:33:26 -0400

    feat: new user tweets render on page automatically - new contract added

--- App/src/MyComponent.js
+++ App/src/MyComponent.js
@@ -7,36 +7,23 @@ import twittor from './twittor'
 // import store from '../../contracts/Twittor.sol';
 
 export default class tweets extends React.Component{
-  constructor(props){
-    super(props);
-  }
 
-  componentWillMount() {
-    const {tweets} = this.props;
-    // const user = tweets;
-    // console.log("user", user);
-    console.log("tweets ", this.props)
-  }
 
   render() {
-        // console.log(tweets['Twittor']['getMessageLength']['0x0'], 'TWEETS')
-      // console.log(web3, 'WEB3')
-      let length =0;
-      console.log('getnumtweets ', this.props.Twittor )
-      if (this.props.Twittor.getNumTweets["0x0"]) {
-        length = this.props.Twittor.getNumTweets["0x0"].value;
+      let length = 0;
+      const userAddress = this.props.accounts[0];
+      const getNumTweetsFirstKey = Object.keys(this.props.Twittor.getNumTweets)[0]
+      if (this.props.Twittor.getNumTweets[getNumTweetsFirstKey]) {
+        length = this.props.Twittor.getNumTweets[getNumTweetsFirstKey].value;
       }
       console.log('length ', length)
       let mapArray = []
       if(length) {
-        // console.log(length.value, 'lENGTH')
     mapArray.length = length
     mapArray.fill(1)  
-    console.log(mapArray, 'lENGTH')
     }
 
-    const userAddress = this.props.accounts[0];
-    console.log("address",  userAddress);
+    
     
       
       return (
@@ -46,11 +33,14 @@ export default class tweets extends React.Component{
       <div>
         <h1>TWEETS</h1>
 
-    <ContractForm contract="Twittor" method="addStructMessage" />
+    <ContractForm contract="Twittor" method="addTweetStruct" />
+   
+  
 
+    
     <div className = "allTweets">
     {mapArray.map((tweet, idx) => {
-      return <div className = "singleTweet"><ContractData contract="Twittor" method="getStructMessage" 
+      return <div className = "singleTweet"><ContractData contract="Twittor" method="getTweetStruct" 
       methodArgs = {[userAddress,idx]}
       key={idx}/></div>
     }).reverse()
@@ -61,10 +51,12 @@ export default class tweets extends React.Component{
     <ContractData contract="Twittor" method="fetchUserTweets" methodArgs = {["0x42d83DC64F5DbDFd4eff8A0B36f7c64ddd24B256"]} />
 
     </div> */}
-    <div className = "hide">
-    {<ContractData  contract="Twittor" method="getNumTweets" methodArgs = {[userAddress]}/>}
 
-    </div>
+<div className = "hide">
+      {<ContractData  contract="Twittor" method="getNumTweets" methodArgs = {[userAddress]}/>}
+
+   </div>
+
 
 
       </div>
--- truffle-config.js
+++ truffle-config.js
@@ -38,7 +38,7 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
-  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
+  contracts_build_directory: path.join(__dirname, "App/src/contracts"),
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
