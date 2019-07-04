0752b2eb56d7a0801b5a6f4d3cab625a428bacf2
Alex Mok <mokalexander00@gmail.com>
2019-07-04 17:14:19 -0400

    feat: working replies

--- App/src/MyComponent.js
+++ App/src/MyComponent.js
@@ -1,134 +1,105 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { ContractData, ContractForm} from 'drizzle-react-components';
-import TweetForm from './TweetForm';
 import SingleTweet from './SingleTweet';
-import { Drizzle } from 'drizzle';
-import options from './drizzleOptions';
-import { DrizzleContext } from "drizzle-react";
 
-// const drizzle = new Drizzle(options);
 export default class tweets extends React.Component {
   constructor(props, context) {
     super(props);
-    this.drizzleState = context.drizzle
-    // this.contractInstance = context.drizzle.contracts.Twittor
-    this.state = { userAddress: '',
-    tweet: "",
-    hashT: "",
-    numTweets: 0,
-    dataKey: null
-  };
-
+    this.drizzleState = context.drizzle;
+    this.state = {
+      userAddress: '',
+      tweet: '',
+      hashT: '',
+      numTweets: 0,
+      dataKey: null,
+    };
   }
   async componentDidMount() {
-    // console.log("drizzle ", drizzle)
-    const {drizzle} = this.props;
-
+    const { drizzle } = this.props;
     const accounts = await this.props.drizzle.web3.eth.getAccounts();
-    // const numTweets = await this.props.drizzle.contracts.Twittor.methods.getNumTweets(accounts[0]).call();
-    
-    // Initializes getNumTweets of store state 
+
+    // Initializes getNumTweets of store state
     // getNumTweets is initially an empty object so this call sets it to however
     // many tweets the current address passed to it has
-    drizzle.contracts.Twittor.methods.getNumTweets.cacheCall(accounts[0])
-    
-    // let numTweets = 0;
-    
-
-    // console.log("numTweets ", numTweets)
+    drizzle.contracts.Twittor.methods.getNumTweets.cacheCall(accounts[0]);
     this.setState({ userAddress: accounts[0] });
-    // const contract = ;
-
-    // drizzle.contracts.Twittor.methods.getNumTweets.cacheCall(accounts[0])
-    // this.setState({dataKey});
-
-    // console.log("KEY ", dataKey)
   }
 
-  handleInputChange = (event) => {
+  handleInputChange = event => {
     this.setState({
       tweet: event.target.value,
       hashT: this.findHashTag(event.target.value),
     });
-    console.log(this.state);
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    console.log(this.state, 'WHAT IS BEING SUBMITTED');
+    await this.props.drizzle.contracts.Twittor.methods
+      .addTweetStruct(this.state.tweet, this.state.hashT)
+      .send({ from: this.state.userAddress });
+  };
+
+  getTweet = async index => {
+    console.log('props ', this.props);
+    const result = await this.props.drizzle.contracts.Twittor.methods
+      .getEverythingTweetStruct(this.state.userAddress, index)
+      .call();
+
+    return result[0];
+  };
+
+  getNum = async index => {
+    const numTweets = await this.props.drizzle.contracts.Twittor.methods
+      .getNumTweets(this.state.userAddress)
+      .call();
+    this.setState({ numTweets });
+    this.forceUpdate();
+  };
+
+  findHashTag(str) {
+    const hashTIndex = str.indexOf('#');
+    let hashT = '';
+
+    if (hashTIndex !== -1) {
+      let endOfHashT = str.indexOf(' ', hashTIndex);
+      if (endOfHashT === -1) endOfHashT = str.length;
+      hashT = str.slice(hashTIndex, endOfHashT);
+    }
+    return hashT || '';
   }
 
-  handleSubmit = async (event) => {
-  event.preventDefault();
-
-
-  await this.props.drizzle.contracts.Twittor.methods.addTweetStruct(this.state.tweet, this.state.hashT).send({from: this.state.userAddress})
-
-  
-
-  //  this.forceUpdate();
-//const numTweets = await this.props.drizzle.contracts.Twittor.methods.getNumTweets(this.state.userAddress).call();
-//this.setState({numTweets });
-
-    
-}
-
- getTweet = async (index) => {
-  console.log("props ", this.props)
-  const result =  await this.props.drizzle.contracts.Twittor.methods.getEverythingTweetStruct(this.state.userAddress, index).call();
-  
-  return result[0];
-}
-
-getNum = async (index) => {
-  console.log("props ", this.props)
-  const numTweets = await this.props.drizzle.contracts.Twittor.methods.getNumTweets(this.state.userAddress).call();
-  this.setState({numTweets})
-  console.log("numTweets", numTweets);
-  this.forceUpdate();
-}
-
-findHashTag(str) {
-  return str.split(' ').filter(word => {
-    return word[0] === '#';
-  })[0];
-}
-
   render() {
-
-    const {drizzleState} = this.props;
+    const { drizzleState } = this.props;
     let length = 0;
 
-    const key = Object.keys(drizzleState.contracts.Twittor.getNumTweets)[0]
+    const key = Object.keys(drizzleState.contracts.Twittor.getNumTweets)[0];
     //if getNumTweets has been initialized then set length to equal getNumTweets
-    if(drizzleState.contracts.Twittor.getNumTweets[key]){  
+    if (drizzleState.contracts.Twittor.getNumTweets[key]) {
       length = drizzleState.contracts.Twittor.getNumTweets[key].value;
     }
 
-    // const getNumTweetsFirstKey = Object.keys(
-    //   this.props.drizzleState.contracts.Twittor.getNumTweets
-    // )[1];
-    // if (this.props.drizzleState.contracts.Twittor.getNumTweets[getNumTweetsFirstKey]) {
-    //   length = this.props.drizzleState.contracts.Twittor.getNumTweets[getNumTweetsFirstKey].value;
-    // }
     let mapArray = [];
     if (length) {
       mapArray.length = length;
       mapArray.fill(1);
     }
-    
 
-    console.log("props", this.props)
-    console.log("drizzleState>>>>", this.props.drizzle.store.getState())
+    // console.log("props", this.props)
+    // console.log("drizzleState>>>>", this.props.drizzle.store.getState())
     // console.log("contractInstance>>>>>", this.contractInstance)
     return (
       <div className="App">
         <ToastContainer />
         {<h1>{length} </h1>}
         <div>
-          <button onClick = {this.getTweet} >GET TWEET</button>
-          <button onClick = {this.getNum} >GET Numtweets</button>
+          <button onClick={this.getTweet}>GET TWEET</button>
+          <button onClick={this.getNum}>GET Numtweets</button>
 
           <h1>TWEETS</h1>
-          <form onSubmit = {this.handleSubmit}>
-          <input
+          <form onSubmit={this.handleSubmit}>
+            <input
               key="tweet"
               name="tweet"
               value={this.state.tweet}
@@ -137,7 +108,6 @@ findHashTag(str) {
             />
             <button type="submit">TWEET</button>
           </form>
-          {/* <TweetForm contract="Twittor" method="addTweetStruct" store = {this.props.drizzle}/> */}
           <div className="allTweets">
             {mapArray
               .map((tweet, idx) => {
@@ -146,39 +116,13 @@ findHashTag(str) {
                     address={this.state.userAddress}
                     index={idx}
                     key={idx}
+                    drizzle={this.props.drizzle}
+                    drizzleState={drizzleState}
                   />
                 );
               })
               .reverse()}
           </div>
-          <div className="hide">
-            {/* {
-              <ContractData
-                contract="Twittor"
-                method="getNumTweets"
-                methodArgs={[this.state.userAddress]}
-              />
-            } */}
-          </div>
-
-          {/* (
-  <DrizzleContext.Consumer>
-    {drizzleContext => {
-      const { drizzle, drizzleState, initialized } = drizzleContext;
-  
-      if (!initialized) {
-        return "Loading...";
-      }
-
-      return (
-      <div>
-      <TweetForm drizzle = {drizzle} drizzleState = {drizzleState} />
-      </div>
-
-      );
-    }}
-  </DrizzleContext.Consumer>
-) */}
         </div>
       </div>
     );
--- App/src/SingleTweet.js
+++ App/src/SingleTweet.js
@@ -1,34 +1,126 @@
 import React from 'react';
-import { Drizzle } from 'drizzle';
-import options from './drizzleOptions';
-const drizzle = new Drizzle(options);
+import ReplyTweet from './ReplyTweet';
 
 export default class SingleTweet extends React.Component {
   constructor(props) {
     super();
 
-    this.state = {};
+    this.state = { reply: '' };
   }
   async componentDidMount() {
-    // call the func here
+    await this.props.drizzle.contracts.Twittor.methods.getNumReplies.cacheCall(
+      this.props.address,
+      this.props.index
+    );
+
     const tweetData = await this.getData(this.props.address, this.props.index);
     this.setState(tweetData);
   }
 
   getData = async (address, index) => {
-    const result = await drizzle.contracts.Twittor.methods
+    const result = await this.props.drizzle.contracts.Twittor.methods
       .getEverythingTweetStruct(address, index)
       .call();
     return result;
   };
 
+  handleInputChange = event => {
+    this.setState({
+      reply: event.target.value,
+    });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    await this.props.drizzle.contracts.Twittor.methods
+      .addReply(this.props.address, this.props.index, this.state.reply)
+      .send({ from: this.props.address });
+  };
+
   render() {
+    const { drizzleState } = this.props;
+    let length = 0;
+    let identifier;
+
+    //Gets list of all single tweet keys
+    const keys = Object.keys(drizzleState.contracts.Twittor.getNumReplies);
+
+    //Searches through the getNumReply arguments, matches the index, and saves indentifier
+    if (keys.length) {
+      for (let i = 0; i < keys.length; i++) {
+        if (
+          drizzleState.contracts.Twittor.getNumReplies[keys[i]].args[1] ===
+          this.props.index
+        ) {
+          identifier = keys[i];
+          break;
+        }
+      }
+
+      //Finds the newly updated num replies
+      if (identifier) {
+        length = drizzleState.contracts.Twittor.getNumReplies[identifier].value;
+      }
+    }
+    let mapArray = [];
+    if (length) {
+      mapArray.length = length;
+      mapArray.fill(1);
+    }
+
     return (
       <div>
         <h1>Single</h1>
         <p>Address: {this.props.address}</p>
         <p>Block Num: {this.state[2]}</p>
-        <p>Replies: {this.state[3]}</p>
+        <p>Replies: {length}</p>
+
+        <div>
+          <button
+            onClick={() =>
+              this.setState({ displayReply: !this.state.displayReply })
+            }
+          >
+            Replies
+          </button>
+          {this.state.displayReply ? (
+            <div>
+              <form onSubmit={this.handleSubmit}>
+                <input
+                  key="reply"
+                  name="reply"
+                  value={this.state.reply}
+                  placeholder="reply"
+                  onChange={this.handleInputChange}
+                />
+                <button type="submit">Reply</button>
+              </form>
+
+              <div>
+                {mapArray.length > 0 ? (
+                  mapArray
+                    .map((tweet, idx) => {
+                      return (
+                        <ReplyTweet
+                          address={this.props.address}
+                          index={this.props.index}
+                          replyIndex={idx}
+                          key={idx}
+                          drizzle={this.props.drizzle}
+                        />
+                      );
+                    })
+                    .reverse()
+                ) : (
+                  <h2>No replies yet</h2>
+                )}
+              </div>
+            </div>
+          ) : (
+            <p />
+          )}
+        </div>
+
         <p>Tweet: {this.state[0]}</p>
       </div>
     );
