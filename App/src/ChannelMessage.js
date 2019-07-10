c452994cb793de8ff4009c94751f8003249a74ae
Michael Ng <ng.michael.m@gmail.com>
2019-07-09 23:03:52 -0400

    added blockie, alias, timestamp

--- App/src/ChannelMessage.js
+++ App/src/ChannelMessage.js
@@ -1,31 +1,58 @@
 import React from 'react';
+import makeBlockie from 'ethereum-blockies-base64';
+import { Popup } from 'semantic-ui-react';
 
 export default class ChannelMessage extends React.Component {
   constructor() {
     super();
-    this.state = { messageData: '', senderAddress: '' };
+    this.state = {
+      messageData: '',
+      senderAddress: '',
+      timeStamp: '',
+      messageAuthorAlias: '',
+    };
   }
   async componentDidMount() {
     const messageData = await this.props.drizzle.contracts.DappChat.methods
       .getReplyData(this.props.channelIndex, this.props.messageIndex)
       .call();
+
+    const messageAuthorAlias = await this.props.drizzle.contracts.DappChat.methods.aliases(messageData[1]);
+
     this.setState({
       messageData: messageData[0],
       senderAddress: messageData[1],
+      timeStamp: messageData[3],
+      messageAuthorAlias,
     });
-    const { channelIndex, messageIndex } = this.props;
     this.props.drizzle.contracts.DappChat.methods.getMessage.cacheCall(
-      channelIndex,
-      messageIndex
+      this.props.channelIndex,
+      this.props.messageIndex,
     );
   }
 
   render() {
+    let blockie;
+    if (this.state.senderAddress) {
+      blockie = (
+        <Popup
+          content={this.state.senderAddress}
+          flowing
+          hoverable
+          trigger={<img alt="blockie" className="blockies" src={makeBlockie(this.state.senderAddress)} />}
+        />
+      );
+    }
     return (
       <div style={{ border: 'solid' }}>
-        <h1>Reply</h1>
-        <p>From: {this.state.senderAddress}</p>
-        <p>Reply: {this.state.messageData}</p>
+        <div>
+          {blockie}
+          <p>{this.state.messageAuthorAlias}</p>
+        </div>
+        <div>
+          <p>{this.state.messageData}</p>
+          <p>{this.state.timeStamp ? this.state.timeStamp : ''}</p>
+        </div>
       </div>
     );
   }
