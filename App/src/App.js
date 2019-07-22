935db4846525ac6df35cef88dfe097df7d8e4476
Alex Mok <mokalexander00@gmail.com>
2019-07-21 22:41:22 -0400

    css:centered initial loading screen and added loader

--- App/src/App.js
+++ App/src/App.js
@@ -2,16 +2,34 @@ import React from 'react';
 import { DrizzleContext } from 'drizzle-react';
 import HomePage from './HomePage';
 import backgroundImage from './assets/Background.max-2800x2800.jpg';
+import { Loader } from 'semantic-ui-react';
+
 export default () => (
   <DrizzleContext.Consumer>
-    {(drizzleContext) => {
+    {drizzleContext => {
       const { drizzle, drizzleState, initialized } = drizzleContext;
 
       if (!initialized) {
-        return <h1>'Loading...'</h1>;
+        return (
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              flexDirection: 'column',
+            }}
+          >
+            <h1>Loading...</h1>
+            <Loader size="massive" active inline />
+          </div>
+        );
       }
       return (
-        <div style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'contain' }}>
+        <div
+          style={{
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundSize: 'contain',
+          }}
+        >
           <HomePage drizzle={drizzle} drizzleState={drizzleState} />
         </div>
       );
